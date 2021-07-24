727c160f0b0beebccb9effe7e7849155435bc1dd
Filip Klimek <87090382+Hackier3@users.noreply.github.com>
2021-07-24 19:20:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const toggleButton = document.getElementsByClassName('hamburger')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('hamburger')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const body = document.getElementsByTagName('body')[0];
+
+function navbar() {
+    const navbar_logos = document.querySelector('.navbar-logo-hamburger');
+    const navbar_styles = getComputedStyle(navbar_logos);
+    var navbar_height = navbar_styles.height;
+    document.getElementsByClassName("navbar-links")[0].style.top = navbar_height;
+}
 
 toggleButton.addEventListener('click', function() {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
+    body.classList.toggle('active');
+    navbar();
 })
